486e42d268d17bc6afe3d4bd9ef69e5b651d3eb6
Jideotetic <jideotetic@gmail.com>
2026-02-02 21:18:06 +0100

    Update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,23 +4,9 @@ import LogoLight from "./LogoLight";
 function Logo({ tag }) {
 	return (
 		<a href="/" className="flex items-center">
-			{tag === "header" && (
-				<>
-					<LogoDark />
-					{/* <img
-						src="/tinker-pal.svg"
-						alt=""
-						className="hidden md:block h-[19.69px]"
-					/> */}
-				</>
-			)}
+			{tag === "header" && <LogoDark />}
 
-			{tag === "footer" && (
-				<>
-					<LogoLight />
-					{/* <img src="/tinker-pal-light.svg" alt="" className="h-[19.69px]" /> */}
-				</>
-			)}
+			{tag === "footer" && <LogoLight />}
 		</a>
 	);
 }
